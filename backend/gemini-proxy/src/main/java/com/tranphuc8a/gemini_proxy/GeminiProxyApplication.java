2d647844c9ae60aa3fa0fc4994cf62a4588e09f8
tranphuc8a <tranphuc8a@gmail.com>
2025-08-25 21:15:24 +0700

    [GP-2] feat: Add base backend project from https://start.spring.io

---
+++ backend/gemini-proxy/src/main/java/com/tranphuc8a/gemini_proxy/GeminiProxyApplication.java
@@ -0,0 +1,13 @@
+package com.tranphuc8a.gemini_proxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeminiProxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeminiProxyApplication.class, args);
+	}
+
+}
---
+++ backend/gemini-proxy/src/test/java/com/tranphuc8a/gemini_proxy/GeminiProxyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tranphuc8a.gemini_proxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeminiProxyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
