67d712707ee38170bdd2ff1f64574abfadd3e98c
tranphuc8a <tranphuc8a@gmail.com>
2025-08-30 15:30:40 +0700

    Fix check style

--- backend/gemini-proxy/src/main/java/com/tranphuc8a/gemini_proxy/GeminiProxyApplication.java
+++ backend/gemini-proxy/src/main/java/com/tranphuc8a/gemini_proxy/GeminiProxyApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GeminiProxyApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GeminiProxyApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GeminiProxyApplication.class, args);
+    }
 
 }
